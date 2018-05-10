10d6f0e1a49e47214da3916fe62e2535ecd2e4fa
Angel Nunez Mencias <git@angelnu.com>
2018-05-10 13:51:37 +0200

    travis removed default apt-get update

--- travis_manifest.sh
+++ travis_manifest.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 
-sudo apt-get install "docker-ce"
+sudo apt-get update
+sudo apt-get install "docker-ce=18.03*"
 
 if [ "x$TRAVIS_BRANCH" = "xmaster" ]; then
   export TAG="latest";
